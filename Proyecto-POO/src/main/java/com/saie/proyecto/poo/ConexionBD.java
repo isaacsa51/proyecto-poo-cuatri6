869555d4bf3c75d19cd6d4b56521391267640d17
Isaac Serrano <serranoei99@gmail.com>
2020-06-17 15:45:21 -0600

    Revert "Implementación del GUI para el login, tiene errores"
    This reverts commit 2cf53cc5635bbd578715b6771700e068c81e7c6d.

--- Proyecto-POO/src/main/java/com/saie/proyecto/poo/ConexionBD.java
+++ Proyecto-POO/src/main/java/com/saie/proyecto/poo/ConexionBD.java
@@ -1,18 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package com.saie.proyecto.poo;
 
 //Importante importar la librería SQL para poder integrar la conexión de una base de datos MySQL al programa
 import java.sql.*;
 
 public class ConexionBD {
-    public Connection conectarBD(){
-        Connection conn;
-        
+    public void conectarBD(){
         //Crear un try catch para saber la razón si falla la conexión a la BD
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             
             //Cambiar la dirección de la base de datos por IP si es necesario
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/multivideo", "root", "");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/multivideo", "root", "");
 
             //Esta sección valida si se puede obtener información de la base de datos y mostrarlos en consola
             /*Statement stnt = conn.createStatement();
@@ -24,20 +28,6 @@ public class ConexionBD {
             }*/
         }catch(Exception error){
             System.out.println(error);
-            conn = null;
-        }
-        
-        return conn;
-    }
-    
-    public void cerrarBD(Connection conBD) throws SQLException{
-        //Checar si esta ya cerrado y si no, cerrar conexión, en caso de error mostrar en consola el causante
-        try{
-            if(conBD.isClosed()){
-                conBD.close();
-            }
-        }catch(SQLException errorCerrar){
-            System.out.println(errorCerrar);
         }
     }
 }
